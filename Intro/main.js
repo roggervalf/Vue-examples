956edba10143f856df8bda1c4cf62a7a3e25062a
rogger andré valverde flores <rogger.valverde@uni.pe>
2020-03-15 13:39:18 -0500

    feat: using attribute binding

--- Intro/main.js
+++ Intro/main.js
@@ -1,9 +1,10 @@
-//Add a description to the data object with the value "A pair of warm, fuzzy socks". Then display the description using an expression in an p element, underneath the h1.
+//Add a link to your data object, and use v-bind to sync it up with an anchor tag in your HTML. Hint: you’ll be binding to the href attribute.
 
 var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of warm fuzzy socks'
+    image: './assets/vmSocks-green-onWhite.jpg',
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
   } 
 })
