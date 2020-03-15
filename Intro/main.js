b9c277e8929fb5423f128543cb21ff594cdf39c7
rogger andré valverde flores <rogger.valverde@uni.pe>
2020-03-15 13:46:37 -0500

    feat: using conditional rendering

--- Intro/main.js
+++ Intro/main.js
@@ -1,10 +1,12 @@
-//Add a link to your data object, and use v-bind to sync it up with an anchor tag in your HTML. Hint: you’ll be binding to the href attribute.
+//Add an onSale property to the data, and use it to conditionally render a span that says “On Sale!”
 
 var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
     image: './assets/vmSocks-green-onWhite.jpg',
-    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+    inStock: true,
+    onSale: true
   } 
 })
