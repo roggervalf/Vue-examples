430be8afd69b297d2002c999f21d7ff976108d48
rogger andré valverde flores <rogger.valverde@uni.pe>
2020-03-15 15:33:02 -0500

    using computed properties

--- Intro/main.js
+++ Intro/main.js
@@ -1,23 +1,25 @@
-//When inStock is false, bind a class to the “Out of Stock” p tag that adds  text-decoration: line-through to that element.
+//Add a new boolean data property `onSale` and create a computed property that takes `brand`, `product` and `onSale` and prints out a string whenever `onSale` is true.
 
 var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    image: './assets/vmSocks-green-onWhite.jpg',
+    brand: 'Vue Mastery',
+    selectedVariant: 0,
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-    inStock: false,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
       {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: './assets/vmSocks-green-onWhite.jpg'
+        variantImage: './assets/vmSocks-green-onWhite.jpg',
+        variantQuantity: 10     
       },
       {
         variantId: 2235,
         variantColor: 'blue',
-        variantImage: './assets/vmSocks-blue-onWhite.jpg'
+        variantImage: './assets/vmSocks-blue-onWhite.jpg',
+        variantQuantity: 0     
       }
     ],
     cart: 0,
@@ -28,11 +30,29 @@ var app = new Vue({
     addToCart() {
       this.cart += 1
     },
-    updateProduct(variantImage) {
-      this.image = variantImage
+    updateProduct(index) {
+      this.selectedVariant = index
     },
     removeFromCart() {
-      this.cart -= 1
+      if(this.cart)
+        this.cart -= 1
+    }
+  },
+  computed: {
+    title() {
+        return this.brand + ' ' + this.product  
+    },
+    image(){
+        return this.variants[this.selectedVariant].variantImage
+    },
+    inStock(){
+        return this.variants[this.selectedVariant].variantQuantity
+    },
+    sale() {
+      if (this.onSale) {
+        return this.brand + ' ' + this.product + ' are on sale!'
+      } 
+        return  this.brand + ' ' + this.product + ' are not on sale'
     }
   }
 })
