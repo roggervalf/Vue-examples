f0b2c8594a431df7b0c46e9d0656c25f2a43666f
rogger andré valverde flores <rogger.valverde@uni.pe>
2020-03-15 19:22:27 -0500

    feat: cleaning repo

--- real-world-vue/src/router/index.js
+++ real-world-vue/src/router/index.js
@@ -1,23 +1,29 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import EventCreate from "../views/EventCreate.vue";
+import EventList from "../views/EventList.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "event-list",
+    component: EventList
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/event",
+    name: "event-show",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/EventShow.vue")
+  },
+  {
+    path: "/event/create",
+    name: "event-create",
+    component: EventCreate
   }
 ];
 
