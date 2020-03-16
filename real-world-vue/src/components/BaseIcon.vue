23a0fc258bc1c2bff3f0198f935fa56dcbb5aab0
rogger andré valverde flores <rogger.valverde@uni.pe>
2020-03-15 22:59:56 -0500

    feat: using slots

--- real-world-vue/src/components/BaseIcon.vue
+++ real-world-vue/src/components/BaseIcon.vue
@@ -2,8 +2,8 @@
   <div class="icon-wrapper">
     <svg class="icon" :width="width" :height="height">
       <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }" />
-      // notice +name here
     </svg>
+    <slot></slot>
   </div>
 </template>
 
